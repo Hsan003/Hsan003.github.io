9ac9927c7f9996dd2b14233ac63bea2146d9dbae
Hsan003 <hsan.ellouze@insat.ucar.tn>
2025-10-02 12:40:49 +0100

    Out folder creation

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: 'export',  // Enable static export
+  basePath: '',      // Leave empty if using custom domain
   eslint: {
     ignoreDuringBuilds: true,
   },
